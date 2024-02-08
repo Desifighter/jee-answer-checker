cbaa7ade890adab28499e3543a847daced4358b4
keshav keshari <kg1718129@gmail.com>
2024-02-08 12:38:51 +0530

    calculation of answer added

--- controllers/landingPageController.js
+++ controllers/landingPageController.js
@@ -1,8 +1,8 @@
 // const axios = require("axios");
 // const cheerio = require("cheerio");
 import axios from "axios";
-import * as cheerio  from "cheerio";
-
+import * as cheerio from "cheerio";
+import xlsx from "xlsx";
 class QuestionParser {
   constructor(text) {
     this.text = text;
@@ -59,42 +59,127 @@ class QuestionParser {
 }
 
 export const landingPageController = async (req, res) => {
-    try {
-        const {url} = req.body;
-        console.log(url);
-        const response = await axios.get(url);
-        const html = response.data;
-        const $ = cheerio.load(html);
-        const menuItems = $(".rw");
+  try {
+    const { url } = req.body;
+    console.log(url);
+    const response = await axios.get(url);
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const menuItems = $(".rw");
 
-        let finalArr = [];
-        
-        menuItems.each((index, element) => {
-          const menuItem = $(element).text().trim();
-          const parser = new QuestionParser(menuItem);
-          parser.sortedOptions = [...parser.optionIds].sort();
-          parser.actualChoosenOption =
-            parser.answerStatus === 1
-              ? parser.sortedOptions.indexOf(
-                  parser.optionIds[Number(parser.chosenOptionId) - 1]
-                ) + 1
-              : null;
-          finalArr.push(parser);
-        });
+    let finalArr = [];
 
-         res.status(200).send({
-           success: true,
-           finalArr
-         });
+    menuItems.each((index, element) => {
+      const menuItem = $(element).text().trim();
+      const parser = new QuestionParser(menuItem);
+      // parser.sortedOptions = [...parser.optionIds].sort();
+      // parser.actualChoosenOption =
+      //   parser.answerStatus === 1
+      //     ? parser.sortedOptions.indexOf(
+      //         parser.optionIds[Number(parser.chosenOptionId) - 1]
+      //       ) + 1
+      //     : null;
 
+      parser.clientAnswer =
+        parser.answerStatus === 1
+          ? Number(parser.optionIds[Number(parser.chosenOptionId) - 1])
+          : null;
+      parser.text = "";
+      finalArr.push(parser);
+    });
 
-    } catch (error) {
-        res.status(400).send({
-            success:false,
-            error
-        })
-        
+    const workbook = xlsx.readFile("answer.xlsx"); // Or exceljs.readFileSync('path/to/your/file.xlsx')
+    const sheetName = workbook.SheetNames[0]; // Assume the first sheet
+    const worksheet = workbook.Sheets[sheetName];
+    const data = xlsx.utils.sheet_to_json(worksheet); // Or exceljs.Worksheet#getData() for full details
+
+    let mathSA = 0;
+    let phySA = 0;
+    let chemSA = 0;
+
+    let index = 0;
+
+    let totalMCQ = 60;
+    let totalAnsweredMCQ = 0;
+    let correctCountMCQ = 0;
+    const map = new Map();
+
+    // console.log(data);
+    for (const row of data) {
+      map.set(row["Question Number"], row["Correct Options/Answers"]);
+      // console.log(row["Question Number"]);
+      // console.log(row["Correct Options/Answers"]);
     }
+
+    for (const row of finalArr) {
+      index++;
+      if (row.questionType === 1 && row.answerStatus === 1) {
+        totalAnsweredMCQ++;
+        if (map.get(Number(row.questionId)) === row.clientAnswer) {
+          correctCountMCQ++;
+        }
+      } else {
+        const check = (one, two) => {
+          console.log("one  :",one,"  two : ",two);
+          if (two == null) {
+            return false;
+          }
+          if (one == two) {
+            return true;
+          }
+          return false;
+        };
+
+        if (index >= 21 && index <= 30) {
+          mathSA = check(map.get(Number(row.questionId)), row.givenAnswer)
+            ? mathSA + 1
+            : mathSA;
+        } else if (index >= 51 && index <= 60) {
+          phySA = check(map.get(Number(row.questionId)), row.givenAnswer)
+            ? phySA + 1
+            : phySA;
+        } else if (index >= 81 && index <= 90) {
+          chemSA = check(map.get(Number(row.questionId)), row.givenAnswer)
+            ? chemSA + 1
+            : chemSA;
+        }
+      }
+    }
+    // console.log(
+    //   totalMCQ,
+    //   "   ",
+    //   totalAnsweredMCQ,
+    //   "   ",
+    //   correctCountMCQ,
+    //   "  ",
+    //   totalAnsweredMCQ - correctCountMCQ
+    // );
+    // console.log(
+    //   map.get(Number(row.questionId)),
+    //   "      ",
+    //   row.clientAnswer
+    // );
+
+    res.status(200).send({
+      success: true,
+      result: {
+        totalMCQ,
+        totalAnsweredMCQ,
+        correctCountMCQ,
+        incorrect: totalAnsweredMCQ - correctCountMCQ,
+        mathSA: (mathSA>5)?5:mathSA,
+        phySA: (phySA>5)?5:phySA,
+        chemSA: (chemSA>5)?5:chemSA,
+        
+      },
+      // finalArr,
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: error,
+    });
+  }
 };
 
 //   axios
